68d9d6d2558601d2368c4c88ff4df867805247f3
Dina Allulu <130566835+dinaallulu@users.noreply.github.com>
2023-04-12 23:44:46 +0200

    Add files via upload

---
+++ SoftwareProjectNews/js/mainNews.js
@@ -0,0 +1,39 @@
+// Variables
+const genralBtn = document.getElementById("genral");
+const businessBtn =  document.getElementById("business");
+const sportBtn = document.getElementById("sport");
+const technologyBtn =  document.getElementById("technology");
+const entertaimentBtn = document.getElementById("entertaiment");
+
+const newsTypeBtn = document.getElementById("newsType");
+const newsDetailsBtn = document.getElementById("newsDetails");
+
+// APIs
+const APIs_KEY = "6a48f4f797c8464498e112e7628e2c8b";
+const HEADLINES_NEWS = "https://newsapi.org/v2/top-headlines?country=in&apiKey=";
+const GENERAL_NEWS = "https://newsapi.org/v2/top-headlines?country=in&category=general&apiKey=";
+const BUSINESS_NEWS = "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=";
+const SPORTS_NEWS = "https://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey=";
+const ENTERTAINMENT_NEWS = "https://newsapi.org/v2/top-headlines?country=in&category=entertainment&apiKey=";
+const TECHNOLOGY_NEWS = "https://newsapi.org/v2/top-headlines?country=in&category=technology&pageSize=8&apiKey=";
+const SEARCH_NEWS = "https://newsapi.org/v2/everything?q=";
+
+genralBtn.addEventListener("click",function(){
+
+});
+
+businessBtn.addEventListener("click",function(){
+
+});
+
+sportBtn.addEventListener("click",function(){
+
+});
+
+technologyBtn.addEventListener("click",function(){
+
+});
+
+entertaimentBtn.addEventListener("click",function(){
+
+});
